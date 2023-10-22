ad21ee1451f2b9af5e5093b9c78627a161cc68d5
Ирина Дарибазарова <99072775+louvenia@users.noreply.github.com>
2023-10-22 12:39:31 +0300

    correction menu

--- ex05/Menu.java
+++ ex05/Menu.java
@@ -6,142 +6,32 @@ public class Menu {
     private final TransactionsService service = new TransactionsService();
     private final Scanner inputItem = new Scanner(System.in);
 
-    private void checkTransactions() {
-        for(Transaction trans : service.validityTransactions()) {
-            System.out.print(trans.getRecipient().getName());
-            System.out.print("(");
-            System.out.print("id = ");
-            System.out.print(trans.getRecipient().getIdentifier());
-            System.out.print(") ");
-            System.out.print("has an unacknowledged transfer id = ");
-            System.out.print(trans.getIdentifier());
-            System.out.print(" from ");
-            System.out.println(trans.getSender().getName());
-            System.out.print("(");
-            System.out.print("id = ");
-            System.out.print(trans.getSender().getIdentifier());
-            System.out.print(") for ");
-            System.out.println(trans.getTransferAmount());
-        }
-    }
-
-    private void removeTransfer() {
-        int userId = inputItem.nextInt();
-        String transferId = inputItem.next();
-        service.removeTransaction(userId, transferId);
-        User user = service.getListUser().getUserId(userId);
-        System.out.print("Transfer ");
-        for(Transaction trans : user.getTransList().toArray()) {
-            if(trans.getIdentifier().toString().equals(transferId)) {
-                if(!trans.getRecipient().equals(service.getListUser().getUserId(userId))) {
-                    System.out.print("To ");
-                    System.out.print(trans.getRecipient().getName());
-                    System.out.print("(");
-                    System.out.print("id = ");
-                    System.out.print(trans.getRecipient().getName());
-                    System.out.print(") ");
-                } else {
-                    System.out.print("From ");
-                    System.out.print(trans.getSender().getName());
-                    System.out.print("(");
-                    System.out.print("id = ");
-                    System.out.print(trans.getSender().getName());
-                    System.out.print(") ");
-                }
-                System.out.print(trans.getTransferAmount());
-                System.out.println(" removed");
-            }
-        }
-    }
-
-    private void userTransactions() {
-        int userId = inputItem.nextInt();
-        for(Transaction trans : service.retrievingTransfer(userId)) {
-            if(!trans.getRecipient().equals(service.getListUser().getUserId(userId))) {
-                System.out.print("To ");
-                System.out.print(trans.getRecipient().getName());
-                System.out.print("(");
-                System.out.print("id = ");
-                System.out.print(trans.getRecipient().getName());
-                System.out.print(") ");
-            } else {
-                System.out.print("From ");
-                System.out.print(trans.getSender().getName());
-                System.out.print("(");
-                System.out.print("id = ");
-                System.out.print(trans.getSender().getName());
-                System.out.print(") ");
-            }
-            System.out.print(trans.getTransferAmount());
-            System.out.print(" with id = ");
-            System.out.println(trans.getIdentifier());
-        }
-    }
-
-    private void performTransfer() {
-        int senderId = inputItem.nextInt();
-        int recipientId = inputItem.nextInt();
-        int amount = inputItem.nextInt();
-        service.perfomingTransaction(recipientId, senderId, amount);
-    }
-
-    private void viewBalance() {
-        int userId = inputItem.nextInt();
-        int balance = service.retrievingBalance(userId);
-        System.out.print(service.getListUser().getUserId(userId).getName());
-        System.out.println(" - " + balance);
-    }
-
-    private void addUser() {
-        System.out.println("Enter a user name and a balance");
-        String[] inputLine = inputItem.nextLine().split("\\s+");
+    public void generalWindow(String launch) {
+        int numberPosition;
+        printInterface(launch);
         try {
-            if(inputLine.length != 2) {
-                throw new RuntimeException("Invalid amount of data entered");
+            numberPosition = checkItem();
+            while((numberPosition != 7 && launch.equals("dev")) || (numberPosition != 5 && launch.equals("production"))) {
+                chooseNumber(numberPosition);
+                printInterface(launch);
+                numberPosition = checkItem();
             }
-            String userName = inputLine[0];
-            int balance = Integer.parseInt(inputLine[1]);
-            service.addUser(userName, balance);
-            System.out.println("User with id = " + service.getListUser().getNumberUsers() + " is added");
-        } catch(RuntimeException e) {
-            e.printStackTrace();
-            addUser();
+        } catch(RuntimeException error) {
+            error.printStackTrace();
+            generalWindow(launch);
         }
+        inputItem.close();
     }
 
-    public void chooseNumber(int number) {
-        switch(number) {
-            case(1):
-                addUser();
-                break;
-            case(2):
-                System.out.println("Enter a user ID");
-                viewBalance();
-                break;
-            case(3):
-                System.out.println("Enter a sender ID, a recipient ID, and a transfer amount");
-                performTransfer();
-                break;
-            case(4):
-                System.out.println("Enter a user ID");
-                userTransactions();
-                break;
-            case(5):
-                System.out.println("Enter a user ID and a transfer ID");
-                removeTransfer();
-                break;
-            case(6):
-                System.out.println("Check results:");
-                checkTransactions();
-                break;
-            default:
-                System.err.println("There is no such item in the menu");
-                break;
+    private int checkItem() {
+        String[] strGen = inputItem.nextLine().trim().split(" ");
+        if(strGen.length != 1) {
+            throw new RuntimeException("You must enter one item number from the menu");
         }
+        return Integer.parseInt(strGen[0]);
     }
 
-    public void printInterface(String launch) {
-        System.out.println("-----------------------------------------------------------------");
+    private void printInterface(String launch) {
         System.out.println("1. Add a user");
         System.out.println("2. View user balances");
         System.out.println("3. Perform a transfer");
@@ -153,6 +43,178 @@ public class Menu {
         } else {
             System.out.println("5. Finish execution");
         }
-        System.out.println("-----------------------------------------------------------------");
+    }
+
+    private void printDelimiter() {
+        System.out.println("---------------------------------------------------------");
+    }
+
+    private void chooseNumber(int number) {
+        switch(number) {
+            case(1):
+                addUser();
+                break;
+            case(2):
+                viewBalance();
+                break;
+            case(3):
+                performTransfer();
+                break;
+            case(4):
+                userTransactions();
+                break;
+            case(5):
+                removeTransfer();
+                break;
+            case(6):
+                checkTransactions();
+                break;
+            default:
+                System.err.println("There is no such item in the menu");
+                printDelimiter();
+                break;
+        }
+    }
+
+    private void addUser() {
+        System.out.println("Enter a user name and a balance");
+        String[] addArr = inputItem.nextLine().trim().split(" ");
+        try {
+            if(addArr.length != 2) {
+                throw new RuntimeException("There must be exactly 2 arguments to the function");
+            }
+            String userName = addArr[0];
+            int balance = Integer.parseInt(addArr[1]);
+            service.addUser(userName, balance);
+            System.out.print("User with id = ");
+            System.out.print(service.getListUser().getNumberUsers());
+            System.out.println(" is added");
+            printDelimiter();
+        } catch(RuntimeException error) {
+            error.printStackTrace();
+            addUser();
+        }
+    }
+
+    private void viewBalance() {
+        System.out.println("Enter a user ID");
+        String[] userIdStr = inputItem.nextLine().trim().split(" ");
+        try {
+            if(userIdStr.length != 1) {
+                throw new RuntimeException("The function uses 1 argument");
+            }
+            int userId = Integer.parseInt(userIdStr[0]);
+            int balance = service.retrievingBalance(userId);
+            System.out.print(service.getListUser().getUserId(userId).getName());
+            System.out.print(" - ");
+            System.out.println(balance);
+            printDelimiter();
+        } catch(RuntimeException error) {
+            error.printStackTrace();
+            viewBalance();
+        }
+    }
+
+    private void performTransfer() {
+        System.out.println("Enter a sender ID, a recipient ID, and a transfer amount");
+        String[] addTransfer = inputItem.nextLine().trim().split(" ");
+        try {
+            if(addTransfer.length != 3) {
+                throw new RuntimeException("There must be exactly 3 arguments to the function");
+            }
+            int senderId = Integer.parseInt(addTransfer[0]);
+            int recipientId = Integer.parseInt(addTransfer[1]);
+            int amount = Integer.parseInt(addTransfer[2]);
+            service.perfomingTransaction(recipientId, senderId, amount);
+            printDelimiter();
+        } catch (RuntimeException error) {
+            error.printStackTrace();
+            performTransfer();
+        }
+    }
+
+    private void infoUserTransaction(Transaction trans, boolean flag) {
+        if(trans.getTransferCategory().equals(Transaction.Category.DEBITS)) {
+            if(flag) {
+                System.out.print("From ");
+            } else {
+                System.out.print(" from ");
+            }
+        } else {
+            if(flag) {
+                System.out.print("To ");
+            } else {
+                System.out.print(" to ");
+            }
+        }
+        System.out.print(trans.getSender().getName());
+        System.out.print("(");
+        System.out.print("id = ");
+        System.out.print(trans.getSender().getIdentifier());
+        System.out.print(") ");
+        if(flag) {
+            System.out.print(trans.getTransferAmount());
+        }
+    }
+
+    private void userTransactions() {
+        System.out.println("Enter a user ID");
+        String[] userStr = inputItem.nextLine().trim().split(" ");
+        try {
+            if(userStr.length != 1) {
+                throw new RuntimeException("The function uses 1 argument");
+            }
+            int userId = Integer.parseInt(userStr[0]);
+            for(Transaction trans : service.retrievingTransfer(userId)) {
+                infoUserTransaction(trans, true);
+                System.out.print(" with id = ");
+                System.out.println(trans.getIdentifier());
+            }
+            printDelimiter();
+        } catch (RuntimeException error) {
+            error.printStackTrace();
+            userTransactions();
+        }
+    }
+
+    private void removeTransfer() {
+        System.out.println("Enter a user ID and a transfer ID");
+        String[] remTrans = inputItem.nextLine().trim().split(" ");
+        try {
+            if(remTrans.length != 2) {
+                throw new RuntimeException("There must be exactly 2 arguments to the function");
+            }
+            int userId = Integer.parseInt(remTrans[0]);
+            String transferId = remTrans[1];
+            System.out.print("Transfer ");
+            for(Transaction trans : service.retrievingTransfer(userId)) {
+                if(trans.getIdentifier().toString().equals(transferId)) {
+                    infoUserTransaction(trans, true);
+                    System.out.println(" removed");
+                }
+            }
+            printDelimiter();
+            service.removeTransaction(userId, transferId);
+        } catch (RuntimeException error) {
+            error.printStackTrace();
+            removeTransfer();
+        }
+    }
+
+    private void checkTransactions() {
+        System.out.println("Check results:");
+        for(Transaction trans : service.validityTransactions()) {
+            System.out.print(trans.getRecipient().getName());
+            System.out.print("(");
+            System.out.print("id = ");
+            System.out.print(trans.getRecipient().getIdentifier());
+            System.out.print(") ");
+            System.out.print("has an unacknowledged transfer id = ");
+            System.out.print(trans.getIdentifier());
+            infoUserTransaction(trans, false);
+            System.out.print("for ");
+            System.out.println(trans.getTransferAmount());
+        }
+        printDelimiter();
     }
 }
--- ex05/Program.java
+++ ex05/Program.java
@@ -1,28 +1,22 @@
 package ex05;
 
-import java.util.Scanner;
-
 public class Program {
+    private static void errorMessage() {
+        System.err.println("Enter application build mode. For example: --profile=dev or --profile=production");
+        System.exit(-1);
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        if(args.length == 0 || !args[0].startsWith("--profile=")) {
-            System.err.println("Enter application build mode. For example: --profile=dev or --profile=production");
-            input.close();
-            System.exit(-1);
+        if(args.length != 1 || !args[0].startsWith("--profile=")) {
+            errorMessage();
         } else {
             String launch = args[0].replace("--profile=", "");
-            int numberPosition;
-            Menu menu = new Menu();
-            menu.printInterface(launch);
-            if(input.hasNextInt()) {
-                numberPosition = input.nextInt();
-                while((numberPosition != 7 && launch.equals("dev")) || (numberPosition != 5 && launch.equals("production"))) {
-                    menu.chooseNumber(numberPosition);
-                    menu.printInterface(launch);
-                    numberPosition = input.nextInt();
-                }
+            if(launch.equals("dev") || launch.equals("production")) {
+                Menu menu = new Menu();
+                menu.generalWindow(launch);
+            } else {
+                errorMessage();
             }
         }
-        input.close();
     }
 }
